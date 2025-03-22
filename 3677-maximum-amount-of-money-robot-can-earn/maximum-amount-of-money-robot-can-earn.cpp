c2e90481bcb6c17fcb7d6a0d9b2f4d5582d13efe
Shivang Bhargava <94477758+shivang0203@users.noreply.github.com>
2025-03-22 15:09:16 +0530

    Time: 391 ms (79.33%) | Memory: 108.5 MB (82.13%) - LeetSync

---
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    int cache[501][501][3];
+    int f(vector<vector<int>>& coins, int i, int j, int neu) {
+        int n = coins.size();
+        int m = coins[0].size();
+        if (i == n - 1 && j == m - 1) {
+            if (neu > 0 && coins[i][j] < 0) {
+                return 0;
+            } else {
+                return coins[i][j];
+            }
+        }
+        if (i >= n || j >= m) {
+            return INT_MIN;
+        }
+        if (cache[i][j][neu] != INT_MIN) {
+            return cache[i][j][neu];
+        }
+        int take = INT_MIN;
+        int nottake = INT_MIN;
+        take =
+            coins[i][j] + max(f(coins, i + 1, j, neu), f(coins, i, j + 1, neu));
+        if (neu > 0 && coins[i][j] < 0)
+            nottake =
+                max(f(coins, i + 1, j, neu - 1), f(coins, i, j + 1, neu - 1));
+        return cache[i][j][neu] = max(take, nottake);
+    }
+
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        for (int i = 0; i < 501; ++i)
+            for (int j = 0; j < 501; ++j)
+                for (int k = 0; k < 3; ++k)
+                    cache[i][j][k] = INT_MIN;
+        return f(coins, 0, 0, 2);
+    }
+};
